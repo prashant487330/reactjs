48886eef761ff8e6541950f9dae01b7d182359bf
prashant patel <prashantpatel487330@gmail.com>
2025-07-22 23:48:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+// const App=()=>{
+//   return(
+//     <>
+//     <h1 style={{color:"red",fontFamily:"areal",textDecoration:"underline"}}>Welcome to react class</h1>
+//     <h2 style={{color:"blue",fontFamily:"verdana"}}>We learn React js</h2>
+//     </>
+//   )
+// }
+// export default App;
+
+
+
+// const App=()=>{
+//   const data={
+//   color:"red",
+//   backgroundColor:"yellow"
+// }
+//   const mydata={
+//     color:"blue",
+//     backgroundColor:"green"
+//   }
+
+//   return(
+//     <>
+//     <h1 style={data}>Welcome to react class</h1>
+//     <h2 style={mydata}>We learn reactjs</h2>
+//     </>
+//   )
+// }
+// export default App;
+
+
+const App=()=>{
+  return(
+    <>
+     <h1>Welcome to react js class for learn reactjs</h1>
+     <div className="data">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus obcaecati deserunt ut earum molestias praesentium quaerat, alias ipsum rerum aut repellendus unde est a. Aspernatur temporibus eius autem voluptate reiciendis.</div>
+     <h2>Our Company Details</h2>
+    </>
+  )
+}
+export default App;
