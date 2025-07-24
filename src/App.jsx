2cb3da7d477986ee714f83fffbd8d745b3c76c5c
prashant patel <prashantpatel487330@gmail.com>
2025-07-24 18:07:32 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,67 @@
+// import image1 from "./images/i1.jpg";
+// import image2 from "./images/12.jpg";
+// import image3 from "./images/i3.jpg";
+// import image4 from "./images/i4.jpg";
+
 // const App=()=>{
 //   return(
 //     <>
-//     <h1 style={{color:"red",fontFamily:"areal",textDecoration:"underline"}}>Welcome to react class</h1>
-//     <h2 style={{color:"blue",fontFamily:"verdana"}}>We learn React js</h2>
+//       <img src={image1} width="300" height="300"/>
+//       <img src={image2} width="300" height="300"/>
+//       <img src={image3} width="300" height="300"/>
+//       <img src={image4} width="300" height="300"/>
 //     </>
 //   )
 // }
 // export default App;
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-
-
-// const App=()=>{
-//   const data={
-//   color:"red",
-//   backgroundColor:"yellow"
-// }
-//   const mydata={
-//     color:"blue",
-//     backgroundColor:"green"
-//   }
-
-//   return(
-//     <>
-//     <h1 style={data}>Welcome to react class</h1>
-//     <h2 style={mydata}>We learn reactjs</h2>
-//     </>
-//   )
-// }
-// export default App;
-
+import Carousel from 'react-bootstrap/Carousel';
+import myimg from "./images/download.jpeg";
+import myimg1 from "./images/download1.jpeg";
+import myimg2 from "./images/download2.jpeg";
 
 const App=()=>{
   return(
     <>
-     <h1>Welcome to react js class for learn reactjs</h1>
-     <div className="data">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus obcaecati deserunt ut earum molestias praesentium quaerat, alias ipsum rerum aut repellendus unde est a. Aspernatur temporibus eius autem voluptate reiciendis.</div>
-     <h2>Our Company Details</h2>
+     <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+         <Carousel fade>
+      <Carousel.Item>
+        <img src={myimg} height="100%" width="100%" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={myimg1} height="100%" width="100%"  />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={myimg2} height="100%" width="100%"  />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     </>
   )
 }
