4e68a9efb97f1f1094db90afa8f7d0f4f87b91e2
prashant patel <prashantpatel487330@gmail.com>
2025-07-26 11:23:35 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,34 @@
-// const App=()=>{
+// import { useState } from "react";
 
-//   const display=(fm,e)=>{
-//     console.log(fm)
-//     console.log(e)
-//     alert(e.type)
+// const App=()=>{
+//   const [name,setName]=useState("ppp");
+//   const myname=()=>{
+//     setName("Prashant")
 //   }
 //   return(
 //     <>
-//      <h1>Welcome to react class</h1>
-//      <button onClick={(e)=>{display("Prashant",e)}}>click here</button>
+//     <h1>Hello i am :{name}</h1>
+//     <button onClick={myname}>Click</button>
+      
 //     </>
 //   )
 // }
 // export default App;
 
-// const App=()=>{
-
-//   const myval=(e)=>{
-//     let mytype=e.type;
-//     let mynm=e.target.name;
-//     let myvalue=e.target.value;
-//     alert(`type:${mytype} name:${mynm} value:${myvalue}`);
-//   }
-//   return(
-//     <>
-//     <h1>hello</h1>
-//     <button name="btn1" value="12" onClick={myval}>Click Here</button>
-//     </>
-//   )
-// }
-// export default App;
-
+import { useState } from "react";
 
 const App=()=>{
-
-  const getval=(e)=>{
-    let name=e.target.name;
-    let value=e.target.value;
-    console.log({[name]:value})
-  }
+  const [color,setColor]=useState("red");
+  
   return(
     <>
-    <h1>hello</h1>
-    Enter name: <input type="text" name="stunm" value="prashant" onChange={getval}/>
+    <h1 style={{color:color}}>My Favroute Color is :{color}</h1>
+    <button onClick={()=>{setColor("pink")}}>Pink</button>
+    <button onClick={()=>{setColor("orange")}}>Orange</button>
+    <button onClick={()=>{setColor("blue")}}>Blue</button>
+    <button onClick={()=>{setColor("green")}}>Green</button>
+      
     </>
   )
 }
 export default App;
-
