7c7defaacfd6af2cbb74c5fd70ec5675940f304a
prashant patel <prashantpatel487330@gmail.com>
2025-07-28 18:19:38 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,19 @@
-import { useEffect, useState } from "react";
-
 const App=()=>{
-  const[count,setCount]=useState(0);
-  // const decrement=()=>{
-  //   if(count<=1){
-  //     alert("Decrement is not possible")
-  //   }
-  //   else{
-  //     setCount(count-1)
-  //   }
-  // }
-
-
-  //  useEffect(()=>{
-  //   setTimeout(()=>{
-  //     setCount(count+1)
-  //   },5000);
-  //  })
-
-
-  //    useEffect(()=>{
-  //   setTimeout(()=>{
-  //     setCount(count+1)
-  //   },5000);
-  //  },[])
-
-  const[multi,setMulti]=useState(0);
-  useEffect(()=>{
-    setMulti(count*2)
-  },[count])
-
   return(
-    // <>
-    // <h1>Counter</h1>
-    // <br />
-    // <h1>{count}</h1>
-    // <button onClick={()=>{setCount(count+1)}}>Increment</button>
-    // <button onClick={decrement}>Decrement</button>
-    // </>
     <>
-    <h1>My Count :{count}</h1>
-    <h2>Multiplication:{multi}</h2>
-
-    <button onClick={()=>{setCount(count+1)}}>Click here</button>
+    <h1>Hello everyone</h1>
+    <h1>I am Prashant</h1>
+    <h1>From Narsinghpur</h1>
+    <h1>Madhya Pradesh</h1>
+    Enter name: <input type="text" />
+    <br />
+    <br />
+    Enter Age: <input type="text" />
+    <hr />
+    Enter Details: <input type="text" />
+    <br />
+    <br />
+    Enter mail: <input type="text" />
     </>
   )
 }
